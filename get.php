04c15926abbfe0b8a4e8cafc9019543119dd3d12
rolandoimmanuel <72653903+rolandoimmanuel@users.noreply.github.com>
2020-10-10 19:02:04 +0700

    Create get.php

---
+++ get.php
@@ -0,0 +1,11 @@
+  <?php
+       echo $_GET['nama'];
+    ?>
+
+    <form action="metode_get.php" method="get">
+        <input type="text" name="nama">
+        <input type="password" name="pas">
+        <input type="submit" name="submit">
+
+
+    </form>
